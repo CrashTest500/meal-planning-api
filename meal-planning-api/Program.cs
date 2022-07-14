2892a5093549dc52dbd8bd16ac3a5096222e8667
apayne1297 <42837001+CrashTest500@users.noreply.github.com>
2022-07-13 21:29:17 -0400

    Adding sample meal options

--- meal-planning-api/Program.cs
+++ meal-planning-api/Program.cs
@@ -37,7 +37,20 @@ app.MapGet("api/meals", () =>
         new() { Id = 4, Name = "Porkchops" },
         new() { Id = 5, Name = "Hamburgers" },
         new() { Id = 6, Name = "Meatball Subs" },
-        new() { Id = 7, Name = "Pancakes" }
+        new() { Id = 7, Name = "Pancakes" },
+        new() { Id = 8, Name = "Salmon" },
+        new() { Id = 9, Name = "Chicken Fried Rice" },
+        new() { Id = 10, Name = "BBQ Chicken" },
+        new() { Id = 11, Name = "Pancakes" },
+        new() { Id = 12, Name = "Tacos" },
+        new() { Id = 13, Name = "Chicken Alfredo" },
+        new() { Id = 14, Name = "Chicken Bacon Ranch Pasta" },
+        new() { Id = 15, Name = "Chicken Noodle Soup" },
+        new() { Id = 16, Name = "Biscuits and Gravy" },
+        new() { Id = 17, Name = "Lo Mein" },
+        new() { Id = 18, Name = "Sweet and Sour Chicken" },
+        new() { Id = 19, Name = "Grilled Cheese" },
+        new() { Id = 20, Name = "Lasagna" }
     };
 
     return meals;
